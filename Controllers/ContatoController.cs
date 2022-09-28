8c5a91d7671d3b3614472b80f2ff01d52c06e9a5
sauloviva <111472017+sauloviva@users.noreply.github.com>
2022-09-28 16:53:17 -0300

    Add files via upload

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControledeContatos.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View(); 
+        }
+
+        public IActionResult ApagarConfirmacao()
+        {
+            return View();
+        }
+    }
+}
+
